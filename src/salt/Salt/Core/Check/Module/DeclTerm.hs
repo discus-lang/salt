7328b579fa433dcd9a615f4fdae6556854643344
Ben Lippmeier <benl@ouroborus.net>
2018-12-27 13:32:32 +1100

    core: check types of term decls against their result annotations

--- src/salt/Salt/Core/Check/Module/DeclTerm.hs
+++ src/salt/Salt/Core/Check/Module/DeclTerm.hs
@@ -25,18 +25,15 @@ checkDeclTermSig _ _ decl
 
 -- | Check bodies of term declarations
 checkDeclTerm :: CheckDecl a
-checkDeclTerm _a ctx (DTerm (DeclTerm a n mpss tResult mBody))
+checkDeclTerm _a ctx (DTerm (DeclTerm a n mpss tsResult mBody))
  = do   let wh   = [WhereTermDecl a n]
         mpss'     <- checkTermParamss a wh ctx mpss
         let ctx' =  foldl (flip contextBindTermParams) ctx mpss'
-        tsResult' <- checkTypesAreAll a wh ctx' TData tResult
+        tsResult' <- checkTypesAreAll a wh ctx' TData tsResult
 
         (mBody', _tsResult, _esResult)
-         <- checkTerm a wh ctx' Synth mBody
+         <- checkTerm a wh ctx' (Check tsResult) mBody
 
-        -- TODO: result type needs to be a vector.
-        -- TODO: check against result type.
-        -- TODO: check result type.
         -- TODO: check effects are empty.
         return  $ DTerm $ DeclTerm a n mpss' tsResult' mBody'
 
