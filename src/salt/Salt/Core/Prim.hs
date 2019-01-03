50b0ac73ebac9ec69cacf7a7c0f988428b6e7bd3
Ben Lippmeier <benl@ouroborus.net>
2019-01-03 21:32:54 +1100

    add stack.yaml to get intero/ghcid support

--- src/salt/Salt/Core/Prim.hs
+++ src/salt/Salt/Core/Prim.hs
@@ -1,10 +1,9 @@
 
 module Salt.Core.Prim
-        ( module Salt.Core.Prim.Data
-        , module Salt.Core.Prim.Ops
+        ( module Salt.Core.Prim.Ops
+        , module Salt.Core.Prim.Ctor
         , module Salt.Core.Prim.Values)
 where
-import Salt.Core.Prim.Data
 import Salt.Core.Prim.Ops
+import Salt.Core.Prim.Ctor
 import Salt.Core.Prim.Values
-
