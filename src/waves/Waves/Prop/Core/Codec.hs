87ce9fc0c4c5e245ab626183597779e68188dd00
Ben Lippmeier <benl@ouroborus.net>
2019-02-06 18:23:31 +1100

    waves: follow parser framework changes

--- src/waves/Waves/Prop/Core/Codec.hs
+++ src/waves/Waves/Prop/Core/Codec.hs
@@ -4,9 +4,9 @@ module Waves.Prop.Core.Codec where
 
 import qualified Salt.Core.Codec.Text.Lexer       as Lexer
 -- import qualified Salt.Core.Codec.Text.Parser      as Parser
-import qualified Salt.Core.Codec.Text.Parser.Base as Parser.Base
+import qualified Salt.Core.Codec.Text.Parser.Base as PB
 import           Salt.Core.Codec.Text.Pretty ()
-import qualified Salt.Core.Codec.Text.Token       as Token
+import qualified Salt.Core.Codec.Text.Token       as K
 import qualified Salt.Core.Transform.StripAnnot   as StripAnnot
 import qualified Text.Parsec                      as Parser
 
@@ -20,7 +20,7 @@ import System.IO.Unsafe (unsafePerformIO)
 -- > dataOfText pTerm . textOfDataPlain = Right
 -- > dataOfText pTerm . textOfDataIndent = Right
 dataOfText :: StripAnnot.StripAnnot c
-           => Parser.Base.Parser (c a) -> String -> Either (RoundtripError (c ())) (c ())
+           => PB.Parser (c a) -> String -> Either (RoundtripError (c ())) (c ())
 dataOfText p text = do
   toks <- scanner text
   parser (StripAnnot.stripAnnot <$> p) toks
@@ -36,7 +36,7 @@ data RoundtripError v
   | ErrorParseLeftover v String
   deriving (Eq, Show)
 
-newtype TokensNoEq = Tokens [Token.At Token.Token]
+newtype TokensNoEq = Tokens [K.At K.Token]
  deriving Show
 
 -- Eq instance for (Either (RoundTripError _) term) is required for
@@ -44,7 +44,7 @@ newtype TokensNoEq = Tokens [Token.At Token.Token]
 instance Eq TokensNoEq where
  _ == _ = True
 
-scanner :: String -> Either (RoundtripError a) [Token.At Token.Token]
+scanner :: String -> Either (RoundtripError a) [K.At K.Token]
 scanner text =
  let -- Is there a pure lexer?
      (toks,_,strRest) = unsafePerformIO $ IW.scanStringIO text Lexer.scanner
@@ -52,8 +52,15 @@ scanner text =
       [] -> return toks
       _  -> Left $ ErrorLexLeftover (Tokens toks) strRest
 
-parser :: Parser.Base.Parser a -> [Token.At Token.Token] -> Either (RoundtripError a) a
+parser :: PB.Parser a -> [K.At K.Token] -> Either (RoundtripError a) a
 parser p toks
- = case Parser.runParser p (IW.Location 0 0) "<test>" toks of
+ = case Parser.runParser p
+                (PB.State
+                { PB.statePrev
+                        = K.At (IW.Range (IW.Location 0 0) (IW.Location 0 0)) K.KMetaStart
+                , PB.stateContext  = []
+                , PB.stateInjected = []})
+        "<test>" toks
+   of
     Right v     -> return v
     Left  err   -> Left $ ErrorNoParse (show err)
