442d1cd3eb236265a68549ddb26ae0702d678eea
Ben Lippmeier <benl@ouroborus.net>
2019-06-04 17:23:49 +1000

    core: start reassoc desugaring in checker

--- src/salt/Salt/Core/Check/Term/App.hs
+++ src/salt/Salt/Core/Check/Term/App.hs
@@ -6,18 +6,6 @@ import qualified Salt.Core.Prim.Ops     as Prim
 import qualified Salt.Core.Prim.Ctor    as Prim
 import qualified Data.Map.Strict        as Map
 
--- import qualified Salt.Core.Transform.StripAnnot         as S
-
--- TODO: we used to have specific error messages for unsaturated prim/ctors
---  pss <- stripTermParamsOfType ctx tPrim
---  when (length pss /= length mgss0)
---   $ throw $ ErrorUnsaturatedPrim a wh nPrm tPrim
-
---  pss <- stripTermParamsOfType ctx tCon'
---  when (length pss /= length mgss0)
---   $ throw $ ErrorUnsaturatedCtor a wh nPrm tCon'
-
-
 -------------------------------------------------------------------------------------------- App --
 -- | Check an application of a term to its arguments.
 checkTermApp
@@ -53,10 +41,26 @@ checkTermApp a wh ctx mFun0 mgss0
         when (null mgss0)
          $ throw $ ErrorAppNoArguments a wh tFun1
 
+        -- Now that we know the type of the function we can look at the
+        -- number of arguments we have and decide whether to desugar
+        -- applications by reassociating them. Doing this accepts applications
+        -- of functions that expect vectors to multiple term arguments,
+        -- which is nicer to write in the source program.
+        let elimsHave = length mgss0
+        mgssReassoc
+         <- if   not $ optionsReassocApps $ contextOptions ctx
+            then return mgss0
+            else do
+                elimsNeeded   <- termElimsOfType a ctx tFun1
+                if   elimsHave > elimsNeeded
+                then reassocApps a ctx tFun1 mgss0
+                else return mgss0
+
+        -- Check the functional expresion against the reassociate arguments.
         (mApp', tsApp', esApp')
          <- checkTermAppArgs
                 a wh ctx
-                a mFun1 tFun1 mgss0
+                a mFun1 tFun1 mgssReassoc
 
         return (mApp', tsApp', esFun1 ++ esApp')
 
@@ -143,16 +147,6 @@ checkTermAppArgs aApp wh ctx aFun mFun tFun mgssArg0
                 (MGAnn aArg (MGTerms msArg') : mgssAcc)
                 mgssRest' (esArg ++ esAcc)
 
-  -- Application of a single term.
-  -- Need to unbake MTerms from MTerm
---   goDispatch (TFun tsParam _tsResult) mgssAcc mgssArg esAcc
---    | (mgs : _mgssRest')  <- mgssArg
---    , Just (_aArg, _mArg) <- takeAnnMGTerm aApp mgs
---    , length tsParam > 1
---    = do mgssArg' <- reassocApps aApp ctx tFun mgssArg
---         putStrLn $ "reassoc " ++ show mgssArg'
---         goDispatch tFun mgssAcc mgssArg' esAcc
-
   goDispatch (TFun tsParam tsResult) mgssAcc mgssArg esAcc
    | (mgs : mgssRest')  <- mgssArg
    , Just (aArg, mArg)  <- takeAnnMGTerm aApp mgs
@@ -183,14 +177,36 @@ checkTermAppArgs aApp wh ctx aFun mFun tFun mgssArg0
 
 
 ---------------------------------------------------------------------------------------------------
+-- | Given the type of a function, determine the number of applications
+--   that would be needed to fully apply it. If we apply it to any more
+--   arguments than this then the function would definately be ill-typed.
+termElimsOfType
+        :: Annot a => a
+        -> Context a -> Type a -> IO Int
+
+termElimsOfType a ctx tFun
+ = do   tFun_red <- simplType a ctx tFun
+        case tFun_red of
+         TFun _ [tResult]
+          -> do n' <- termElimsOfType a ctx tResult
+                return $ 1 + n'
+
+         TFun _ _ -> return 1
+
+         _ -> return 0
+
+
+-- | Reassociate arguments into vectors to satify the application of a
+--   function with the given type.
 reassocApps
-        :: Annot a
-        => a -> Context a -> Type a
+        :: Annot a => a
+        -> Context a -> Type a
         -> [TermArgs a] -> IO [TermArgs a]
+
 reassocApps a ctx tFun mgss
  = do   tFun_red <- simplType a ctx tFun
         case takeTFun tFun_red of
-         Nothing        -> return mgss
+         Nothing -> return mgss
          Just (tsParam, _tsResult)
           | length tsParam > 1
           , Just (msArg, mgss') <- takeSomeMGTerm (length tsParam) mgss
@@ -199,8 +215,8 @@ reassocApps a ctx tFun mgss
           | otherwise -> return mgss
 
 
--- | Try to take the given number of MGTerm arguments
---   from the front of an arguments list.
+-- | Try to take the given number of MGTerm arguments from the front
+--   of an arguments list.
 --   TODO: preserve annotations.
 takeSomeMGTerm :: Int -> [TermArgs a] -> Maybe ([Term a], [TermArgs a])
 takeSomeMGTerm 0 mgss
