fb37dccf213196306501f9dc528813c0d4586368
Amos Robinson <amos.robinson@gmail.com>
2020-08-02 22:00:29 +1000

    trivial: upgrade parsec to fix ci
    The Travis build failed to install Parsec because a newer version was
    already installed. Might as well just try the newer version.

--- make/target/setup.mk
+++ make/target/setup.mk
@@ -14,7 +14,7 @@ setup :
 		text mtl stm json \
 		happy-1.19.9
 	@$(DEPS_INSTALLER) v1-install \
-		parsec-3.1.13.0 \
+		parsec-3.1.14.0 \
 		inchworm-1.1.1.2 \
 		buildbox-2.2.1.2 \
 		hedgehog-0.6.1
